6262f67b77efd79fe846124b07ce45024dab52a5
yashchouhan <yashchouhan@192.168.43.19>
2019-11-06 00:56:37 +0530

    maven codes

--- Selenium_PageObjectModel/src/main/java/com/crm/qa/util/TestUtil.java
+++ Selenium_PageObjectModel/src/main/java/com/crm/qa/util/TestUtil.java
@@ -1,7 +1,31 @@
 package com.crm.qa.util;
 
-public class TestUtil {
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.crm.qa.base.TestBase;
+
+public class TestUtil extends TestBase{
+
+	public TestUtil() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 
 	public static long PAGE_LOAD_TIMEOUT= 20;
 	public static long IMPLICIT_WAIT= 10;
+	
+	
+	public static void takeScreenshotAtEndOfTest() throws IOException {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+	}
+
+
 }
